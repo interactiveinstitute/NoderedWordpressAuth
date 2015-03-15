3e47dfa76435dfb7aba7e796d740afa2b66eaa87
Anton04 <anton.gustafsson@tii.se>
2015-03-15 09:57:27 +0100

    Update WordpressAuth.py

--- WordpressAuth.py
+++ WordpressAuth.py
@@ -13,6 +13,42 @@ import MySQLdb
 def auth(username,password):
     
     return
+
+def GetWPuser():
+    cred = GetWPDBcredentials()
+    
+    try:
+        con = MySQLdb.connect(cred['DB_HOST'], cred['DB_USER'], cred['DB_PASSWORD'], cred['DB_NAME'])
+        cur = con.cursor()
+        cur.execute("""
+            SELECT ID,user_login,user_pass FROM wp_users 
+        """)
+        
+        users = cur.fetchall()
+        
+        cur.execute("""
+            SELECT user_id,meta_value FROM wordpress.wp_usermeta where meta_key="wp_capabilities";
+        """)
+
+        cap = cur.fetchall()
+        
+    except MySQLdb.Error, e:
+        print "Error %d: %s" % (e.args[0], e.args[1])
+    finally:
+        if con:
+            con.close()
+            
+    u_dict = {}
+
+    for  (u_id,u_name,u_hash) in users:
+        for (cap_id,u_cap) in cap:
+            if cap_id == u_id:
+                break;
+    
+        u_cap = u_cap.split('"')[1]
+        u_dict[u_name] = [u_id,u_cap,u_hash]
+    
+    return u_dict 
     
 def GetWPDBcredentials(config_file="/var/www/wp-config.php"):
     cred = {}
@@ -27,7 +63,7 @@ def GetWPDBcredentials(config_file="/var/www/wp-config.php"):
             parts = l.split("'")
             cred[parts[1]] = parts[3]
             
-    
+
     
     return cred
 
